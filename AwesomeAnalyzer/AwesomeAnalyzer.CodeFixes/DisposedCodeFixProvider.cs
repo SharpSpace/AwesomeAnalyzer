3e6cf974f610f5656f9958ca40ec430e194be36d
Jerry <Jerry.jo84@gmail.com>
2022-11-11 20:08:15 +0100

    #3 Using statement only works after C# 8.0

--- AwesomeAnalyzer/AwesomeAnalyzer.CodeFixes/DisposedCodeFixProvider.cs
+++ AwesomeAnalyzer/AwesomeAnalyzer.CodeFixes/DisposedCodeFixProvider.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Text;
 
 namespace AwesomeAnalyzer
@@ -20,7 +24,9 @@ namespace AwesomeAnalyzer
 
         public override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
+            //context.Document.Project.CompilationOptions
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var languageVersion = ((CSharpParseOptions)(await context.Document.GetSyntaxTreeAsync(context.CancellationToken))?.Options)?.LanguageVersion ?? LanguageVersion.CSharp6;
 
             foreach (var diagnostic in context.Diagnostics)
             {
@@ -29,11 +35,11 @@ namespace AwesomeAnalyzer
                     .AncestorsAndSelf()
                     .OfType<LocalDeclarationStatementSyntax>()
                     .First();
-
+                
                 context.RegisterCodeFix(
                     CodeAction.Create(
                         "Add using to statement",
-                        token => AddUsingAsync(context.Document, declaration, token),
+                        token => AddUsingAsync(context.Document, declaration, languageVersion, token),
                         equivalenceKey: "DisposedCodeFixTitle"
                     ),
                     diagnostic
@@ -41,14 +47,49 @@ namespace AwesomeAnalyzer
             }
         }
 
-        private async Task<Document> AddUsingAsync(Document document, LocalDeclarationStatementSyntax declaration, CancellationToken token)
+        private async Task<Document> AddUsingAsync(
+            Document document, 
+            LocalDeclarationStatementSyntax declaration,
+            LanguageVersion languageVersion, 
+            CancellationToken token
+        )
         {
             if (declaration.UsingKeyword.ValueText == "using") return document;
 
-            var oldSource = (await document.GetSyntaxRootAsync(token).ConfigureAwait(false)).ToFullString();
-            var newSource = oldSource.Insert(declaration.SpanStart, "using ");
+            if (languageVersion <= LanguageVersion.CSharp8)
+            {
+                var children = declaration.Parent.ChildNodes().Where(x => x.FullSpan.Start >= declaration.FullSpan.End).ToList();
 
-            return document.WithText(SourceText.From(newSource));
+                var oldSource = (await document.GetSyntaxRootAsync(token).ConfigureAwait(false)).ToFullString();
+
+                var newSource = oldSource;
+                var blockCode = string.Empty;
+                
+                if (children.Any())
+                {
+                    var startIndex = children.Min(x => x.SpanStart);
+                    var endIndex = children.Max(x => x.FullSpan.End);
+                    blockCode = oldSource.Substring(startIndex, endIndex - startIndex);
+                    newSource = oldSource.Remove(startIndex, endIndex - startIndex);
+                }
+
+                newSource = newSource.Remove(declaration.FullSpan.End - 3, 3);
+                newSource = newSource.Insert(declaration.FullSpan.End - 3, $@")
+        {{
+            {blockCode}}}"
+                );
+
+                newSource = newSource.Insert(declaration.SpanStart, "using (");
+
+                return await Formatter.FormatAsync(document.WithText(SourceText.From(newSource)), cancellationToken: token);
+            }
+            else
+            {
+                var oldSource = (await document.GetSyntaxRootAsync(token).ConfigureAwait(false)).ToFullString();
+                var newSource = oldSource.Insert(declaration.SpanStart, "using ");
+
+                return document.WithText(SourceText.From(newSource));
+            }
         }
     }
 }
--- AwesomeAnalyzer/AwesomeAnalyzer.Test/DisposeTest.cs
+++ AwesomeAnalyzer/AwesomeAnalyzer.Test/DisposeTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using VerifyCS = AwesomeAnalyzer.Test.CSharpCodeFixVerifier<
@@ -72,6 +73,82 @@ namespace MyNamespace
         private void A() 
         {
             using var reader = new StreamReader("""");
+        }
+    }
+}");
+        }
+
+        [TestMethod]
+        public async Task Test2_Diagnostic()
+        {
+            await VerifyCS.VerifyCodeFixAsync(
+                LanguageVersion.CSharp8, 
+                @"
+using System.IO;
+
+namespace MyNamespace
+{
+    public sealed class Program
+    {
+        private void A()
+        {
+            [|var reader = new StreamReader("""")|];
+        }
+    }
+}", fixedSource: @"
+using System.IO;
+
+namespace MyNamespace
+{
+    public sealed class Program
+    {
+        private void A()
+        {
+            using (var reader = new StreamReader(""""))
+            {
+            }
+        }
+    }
+}");
+        }
+
+        [TestMethod]
+        public async Task Test3_Diagnostic()
+        {
+            await VerifyCS.VerifyCodeFixAsync(
+                LanguageVersion.CSharp8,
+                @"
+using System.IO;
+
+namespace MyNamespace
+{
+    public sealed class Program
+    {
+        private void A()
+        {
+            var b = string.Empty;
+            [|var reader = new StreamReader("""")|];
+            var a = string.Empty;
+
+            var c = string.Empty;
+        }
+    }
+}", fixedSource: @"
+using System.IO;
+
+namespace MyNamespace
+{
+    public sealed class Program
+    {
+        private void A()
+        {
+            var b = string.Empty;
+            using (var reader = new StreamReader(""""))
+            {
+                var a = string.Empty;
+
+                var c = string.Empty;
+            }
         }
     }
 }");
